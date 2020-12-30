be4cd7b501ca939d7842a0460cb7d81193f77f40
Eozef <eozefow@gmail.com>
2020-12-30 22:48:51 +1100

    initialize the Card

---
+++ src/Card.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Card = () => {
+    return (
+        <div>
+            <img alt='roboImage' src='https://robohash.org/a'></img>
+            <h2>Joe</h2>
+            <p>Joe@gmail.com</p>
+        </div>
+    );
+}
+
+export default Card;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Card from './Card';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+
+  <Card />, document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
