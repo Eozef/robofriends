e748d2285bf10f3a2729c3d97ca5bb58ce9ddc7c
Eozef <eozefow@gmail.com>
2020-12-31 00:23:06 +1100

    refactoring code

--- src/Card.js
+++ src/Card.js
@@ -1,12 +1,13 @@
 import React from 'react';
 
 const Card = (props) => {
+    const { id, name, email } = props;
     return (
         <div className='tc bg-light-blue dib br4 pa3 ma2 grow bw3 shadow-5'>
-            <img alt='roboImage' src={`https://robohash.org/${props.id}?300x300`}></img>
+            <img alt='roboImage' src={`https://robohash.org/${id}?300x300`}></img>
             <div>
-                <h2>{props.name}</h2>
-                <p>{props.email}</p>
+                <h2>{name}</h2>
+                <p>{email}</p>
             </div>
         </div>
     );
