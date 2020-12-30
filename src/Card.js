644c28f7699a9ca98975503413220c2d0e379d52
Eozef <eozefow@gmail.com>
2020-12-31 00:33:00 +1100

    refactorying code

--- src/Card.js
+++ src/Card.js
@@ -2,13 +2,11 @@ import React from 'react';
 
 const Card = ({ id, name, email }) => {
     return (
-        <div className='tc bg-light-blue dib br4 pa3 ma2 grow bw3 shadow-5'>
+        <React.Fragment className='tc bg-light-blue dib br4 pa3 ma2 grow bw3 shadow-5'>
             <img alt='roboImage' src={`https://robohash.org/${id}?300x300`}></img>
-            <div>
-                <h2>{name}</h2>
-                <p>{email}</p>
-            </div>
-        </div>
+            <h2>{name}</h2>
+            <p>{email}</p>
+        </React.Fragment>
     );
 }
 
